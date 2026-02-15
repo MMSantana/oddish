b93c72e15fe699b2e69fc7d8a3966387ff43a043
mmsantana <manuel@not.com>
2026-02-14 21:44:17 -0300

    Fixes migration file

--- priv/repo/migrations/20260213233252_fix_pack_history.exs
+++ priv/repo/migrations/20260213233252_fix_pack_history.exs
@@ -1,13 +1,37 @@
 defmodule Oddish.Repo.Migrations.FixPackHistory do
   use Ecto.Migration
 
-  def change do
-    rename table(:bovine_pack_history), to: table(:bovine_pack_histories)
+  def up do
+    execute "DROP TABLE IF EXISTS bovine_pack_history CASCADE"
 
-    alter table(:bovine_pack_histories) do
+    create table(:bovine_pack_histories) do
+      add :bovine_id, references(:bovines, on_delete: :nilify_all)
+      add :pack_id, references(:packs, on_delete: :nilify_all)
+      add :start_date, :utc_datetime_usec
+      add :end_date, :utc_datetime_usec
       add :org_id, references(:organizations, type: :id, on_delete: :delete_all)
+
+      timestamps(type: :utc_datetime)
     end
 
+    create index(:bovine_pack_histories, [:bovine_id])
+    create index(:bovine_pack_histories, [:pack_id])
+
     create unique_index(:soltas, [:org_id, :name], name: :soltas_unique_name_index)
+  end
+
+  def down do
+
+    create table(:bovine_pack_history) do
+      add :bovine_id, references(:bovines, on_delete: :nilify_all)
+      add :pack_id, references(:packs, on_delete: :nilify_all)
+      add :start_date, :utc_datetime_usec
+      add :end_date, :utc_datetime_usec
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create index(:bovine_pack_history, [:bovine_id])
+    create index(:bovine_pack_history, [:pack_id])
   end
 end
